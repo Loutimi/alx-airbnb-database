392f0dfef88e44403ecfdee7311cbb4b2c5c3d09
Loutimi <loutimi59@gmail.com>
2025-07-01 16:48:23 +0100

    Add SQL queries to demonstrate various JOIN operations

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,31 @@
+-- INNER JOIN query to retrieve all bookings and the respective users who made those bookings.
+SELECT 
+    u.first_name, 
+    u.last_name, 
+    b.property_id
+FROM "User" AS u
+INNER JOIN "Booking" AS b
+    ON u.user_id = b.user_id;
+
+
+-- LEFT JOIN query to retrieve all properties and their reviews, including properties that have no reviews.
+SELECT 
+    prop.name,  
+    prop.property_id,
+    r.rating
+FROM "Property" AS prop
+LEFT JOIN "Review" AS r
+    ON prop.property_id = r.property_id
+ORDER BY prop.name;
+
+
+/* FULL OUTER JOIN query to retrieve all users and all bookings,
+   even if the user has no booking or a booking is not linked to a user.
+ */
+SELECT 
+    u.first_name, 
+    u.last_name, 
+    b.property_id
+FROM "User" AS u
+FULL OUTER JOIN "Booking" AS b
+    ON u.user_id = b.user_id;
